2f00905be960590ed201e17c85108423d42191a4
Wim Van Laer <van.laer.wim@gmail.com>
2023-12-11 18:21:43 +0100

    solved day 9 part 1

---
+++ day09/puzzle1/puzzle1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(Solve(os.Stdin))
+}
+
+func Solve(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	sum := 0
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		hist := ParseHistory(l)
+		sum += Extrapolate(hist)
+	}
+
+	return sum
+}
+
+func ParseHistory(s string) []int {
+	ss := strings.Split(s, " ")
+	hist := make([]int, len(ss))
+	for i := range ss {
+		n, err := strconv.Atoi(ss[i])
+		if err != nil {
+			panic(err)
+		}
+		hist[i] = n
+	}
+	return hist
+}
+
+func Extrapolate(ns []int) int {
+	difs := make([]int, len(ns)-1)
+	allZero := true
+	for i := 0; i < len(ns)-1; i++ {
+		difs[i] = ns[i+1] - ns[i]
+		if difs[i] != 0 {
+			allZero = false
+		}
+	}
+	if !allZero {
+		nextDif := Extrapolate(difs)
+		return ns[len(ns)-1] + nextDif
+	} else {
+		return ns[len(ns)-1]
+	}
+}
---
+++ day09/puzzle1/puzzle1_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSolve(t *testing.T) {
+	input :=
+		`0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`
+	const expected = 114
+
+	output := Solve(strings.NewReader(input))
+	if output != expected {
+		t.Fatalf("expected output to be %v but got %v", expected, output)
+	}
+}
